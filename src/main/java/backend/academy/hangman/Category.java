30d386332adbfa6b9508bc4ae87545472132b817
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-09-12 18:06:48 +0300

    feat: Add method to get random category

--- src/main/java/backend/academy/hangman/Category.java
+++ src/main/java/backend/academy/hangman/Category.java
@@ -1,5 +1,11 @@
 package backend.academy.hangman;
 
+import java.util.Random;
+
 public enum Category {
     ANIMALS, CITIES, FRUITS, COUNTRIES, SPORTS;
+
+    public static Category getRandomCategory() {
+        return values()[new Random().nextInt(values().length)];
+    }
 }
